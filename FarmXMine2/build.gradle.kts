04005533952da9f81de9f38bc61b55cabafc9d42
Leon210519 <leon.littwins@gmail.com>
2025-09-05 17:58:20 +0200

    Add artifact discovery with economy boost

--- FarmXMine2/build.gradle.kts
+++ FarmXMine2/build.gradle.kts
@@ -14,10 +14,14 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
+        exclude(group = "org.bukkit")
+    }
 }
 
